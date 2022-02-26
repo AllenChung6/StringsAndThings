feb4ca67638ac01217f2c7ed78692328a7e122d4
Allen C <allenc@Zipcoders-MacBook-Pro.local>
2022-02-25 20:00:21 -0500

    Finished the removeString method. Tests passed

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -1,5 +1,5 @@
 package io.zipcoder;
-
+import java.lang.Character;
 
 /**
  * @author tariq
@@ -11,58 +11,81 @@ public class StringsAndThings {
      * but not the 'y' in "yellow" (not case sensitive). We'll say that a y or z is at the end of a word if there is not an alphabetic
      * letter immediately following it. (Note: Character.isLetter(char) tests if a char is an alphabetic letter.)
      * example : countYZ("fez day"); // Should return 2
-     *           countYZ("day fez"); // Should return 2
-     *           countYZ("day fyyyz"); // Should return 2
+     * countYZ("day fez"); // Should return 2
+     * countYZ("day fyyyz"); // Should return 2
      */
-    public Integer countYZ(String input){
-        return null;
-    }
+    public Integer countYZ(String input) {
+        String word = input;
+        int count = 0;
+        int finalCounter;
 
-    /**
-     * Given two strings, base and remove, return a version of the base string where all instances of the remove string have
-     * been removed (not case sensitive). You may assume that the remove string is length 1 or more.
-     * Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
-     *
-     * example : removeString("Hello there", "llo") // Should return "He there"
-     *           removeString("Hello there", "e") //  Should return "Hllo thr"
-     *           removeString("Hello there", "x") // Should return "Hello there"
-     */
-    public String removeString(String base, String remove){
-        return null;
-    }
+        word.substring(word.length() -1);
+        System.out.println(word);
 
-    /**
-     * Given a string, return true if the number of appearances of "is" anywhere in the string is equal
-     * to the number of appearances of "not" anywhere in the string (case sensitive)
-     *
-     * example : containsEqualNumberOfIsAndNot("This is not")  // Should return false
-     *           containsEqualNumberOfIsAndNot("This is notnot") // Should return true
-     *           containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
-     */
-    public Boolean containsEqualNumberOfIsAndNot(String input){
-        return null;
-    }
+        for (int i = 0; i <= word.length(); i++) {
+            char currentChar = word.charAt(i);
+            //System.out.println(currentChar);
 
-    /**
-     * We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
-     * Return true if all the g's in the given string are happy.
-     * example : gHappy("xxggxx") // Should return  true
-     *           gHappy("xxgxx") // Should return  false
-     *           gHappy("xxggyygxx") // Should return  false
-     */
-    public Boolean gIsHappy(String input){
-        return null;
-    }
+            if ((currentChar == 'y') || (currentChar == 'z')) {
+                count++;
+                System.out.println(count);
+            }
+            }
+        return count;
+        }
 
 
-    /**
-     * We'll say that a "triple" in a string is a char appearing three times in a row.
-     * Return the number of triples in the given string. The triples may overlap.
-     * example :  countTriple("abcXXXabc") // Should return 1
-     *            countTriple("xxxabyyyycd") // Should return 3
-     *            countTriple("a") // Should return 0
-     */
-    public Integer countTriple(String input){
-        return null;
+
+
+        /**
+         * Given two strings, base and remove, return a version of the base string where all instances of the remove string have
+         * been removed (not case sensitive). You may assume that the remove string is length 1 or more.
+         * Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
+         *
+         * example : removeString("Hello there", "llo") // Should return "He there"
+         *           removeString("Hello there", "e") //  Should return "Hllo thr"
+         *           removeString("Hello there", "x") // Should return "Hello there"
+         */
+        public String removeString(String base, String remove){
+            String removedString = base.replace(remove, "");
+            return removedString;
+        }
+
+        /**
+         * Given a string, return true if the number of appearances of "is" anywhere in the string is equal
+         * to the number of appearances of "not" anywhere in the string (case sensitive)
+         *
+         * example : containsEqualNumberOfIsAndNot("This is not")  // Should return false
+         *           containsEqualNumberOfIsAndNot("This is notnot") // Should return true
+         *           containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
+         */
+
+
+        public Boolean containsEqualNumberOfIsAndNot (String input){
+            return null;
+        }
+
+        /**
+         * We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
+         * Return true if all the g's in the given string are happy.
+         * example : gHappy("xxggxx") // Should return  true
+         *           gHappy("xxgxx") // Should return  false
+         *           gHappy("xxggyygxx") // Should return  false
+         */
+        public Boolean gIsHappy (String input){
+            return null;
+        }
+
+
+        /**
+         * We'll say that a "triple" in a string is a char appearing three times in a row.
+         * Return the number of triples in the given string. The triples may overlap.
+         * example :  countTriple("abcXXXabc") // Should return 1
+         *            countTriple("xxxabyyyycd") // Should return 3
+         *            countTriple("a") // Should return 0
+         */
+        public Integer countTriple (String input){
+            return null;
+        }
     }
-}
+
